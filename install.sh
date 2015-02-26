e12bdd64e010ff96b11ee119ff61682a83ae4d9f
Victor Gan <victorgan@gmail.com>
2015-02-25 16:17:10 -0800

    Zero'd out initially

--- install.sh
+++ install.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
 # Get current dir (so run this script from anywhere)
-
 export DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
-
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
 # Bunch of symlinks
-
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/etc/mackup/.mackup.cfg" ~
-
-if [ "$(uname)" == "Darwin" -a -f "$DOTFILES_DIR/install/osx.sh" ]; then
-    source "$DOTFILES_DIR/install/osx.sh"
-fi
+# ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+# ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
+# ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+# ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
